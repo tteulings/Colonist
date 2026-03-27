3035cfff9b3119450fbc9036196f031246faae57
Tijs Teulings <tijs.t@hotmail.com>
2026-03-27 08:42:10 +0100

    Cache bust icon paths so new SVGs load (v=56)

--- src/game.js
+++ src/game.js
@@ -2,11 +2,11 @@ const RESOURCES = ["wood", "brick", "sheep", "wheat", "ore"];
 const RESOURCE_SHORT = { wood: "Wd", brick: "Br", sheep: "Sh", wheat: "Wh", ore: "Or" };
 const RESOURCE_LABEL = { wood: "Wood", brick: "Brick", sheep: "Sheep", wheat: "Wheat", ore: "Ore" };
 const RESOURCE_ICON_PATH = {
-  wood: "./assets/icons/resource-wood.svg",
-  brick: "./assets/icons/resource-brick.svg",
-  sheep: "./assets/icons/resource-sheep.svg",
-  wheat: "./assets/icons/resource-wheat.svg",
-  ore: "./assets/icons/resource-ore.svg",
+  wood: "./assets/icons/resource-wood.svg?v=56",
+  brick: "./assets/icons/resource-brick.svg?v=56",
+  sheep: "./assets/icons/resource-sheep.svg?v=56",
+  wheat: "./assets/icons/resource-wheat.svg?v=56",
+  ore: "./assets/icons/resource-ore.svg?v=56",
 };
 const RESOURCE_COLORS = {
   wood: "#4a7c3f",
